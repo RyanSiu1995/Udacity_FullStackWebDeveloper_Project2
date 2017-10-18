9ec26ecf03c5a28ed8b216ee0147de9953635766
Ryan Siu <ryansiu@uw.edu>
2017-10-19 00:13:46 +0800

    first commit

---
+++ js/showcase.js
@@ -0,0 +1,17 @@
+$(document).ready(e => {
+	var showcase = new Vue({
+		el: '#show-case',
+		data : {
+			projects: [
+				{
+					title: 'Academic',
+					body: 'text',
+					github: 'http',
+					version: '',
+					remarks: ''
+				}
+			] 
+		}
+	})
+})
+
