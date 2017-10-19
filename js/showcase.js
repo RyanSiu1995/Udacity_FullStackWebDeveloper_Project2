dc94bdf710d7360542f146b3fecb4bc7dffe4cf1
Ryan Siu <ryansiu@uw.edu>
2017-10-19 15:02:36 +0800

    Create the showcase with VueJS

--- js/showcase.js
+++ js/showcase.js
@@ -1,17 +1,36 @@
 $(document).ready(e => {
 	var showcase = new Vue({
 		el: '#show-case',
-		data : {
-			projects: [
-				{
-					title: 'Academic',
-					body: 'text',
-					github: 'http',
-					version: '',
-					remarks: ''
-				}
-			] 
+		data : function() {
+			return {
+				projects: [
+					{
+						id: 1,
+						title: "CKEditor Image Uploader Plugin",
+						github: "ckeditor-ImageUploader",
+						image: "images/showcase/ckeditor.png",
+						detail: "Plugin enables ckeditor to upload image without file browser"
+					},
+					{
+						id: 2,
+						title: "Python Interactive Shell",
+						github: "Python-In-NodeJS",
+						image: "images/showcase/pythonshell.png",
+						detail: "Python Interactive Shell in Socket.io and NodeJS"
+					},
+					{
+						id: 3,
+						title: "Astrophysical Fluid Stimulation",
+						github: "Fluid-Stimulation",
+						image: "images/showcase/simulation.png",
+						report: "https://goo.gl/y5anBQ",
+						detail: "Astrophysical fluid stimuation's implementation in Java"
+					}
+				]
+			}
 		}
 	})
+
+	console.log(showcase)
 })
 
